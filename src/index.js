dd6ddf2b8096fb692d0f8e1b8779929779762700
kstewar1 <77819538+kstewar1@users.noreply.github.com>
2022-10-08 13:38:35 -0400

    tidying up

--- src/index.js
+++ src/index.js
@@ -4,6 +4,3 @@ import "./index.css";
 
 import {Board} from "./modules/board";
 ReactDOM.render(<Board/>, document.getElementById("root"));
-
-// import {Board2} from "./modules/board2";
-// ReactDOM.render(<Board2/>, document.getElementById("root"));
