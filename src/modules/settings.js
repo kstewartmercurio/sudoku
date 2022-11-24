1ba62504c03c3f04d6fab9c5195c51db101f91a8
kstewar1 <77819538+kstewar1@users.noreply.github.com>
2022-11-23 23:40:37 -0500

    started settings page

--- src/modules/settings.js
+++ src/modules/settings.js
@@ -4,8 +4,13 @@ export function Settings() {
     return (
         <>
             <div className="section" id="settings-page">
+                <div id="settings-nav-column">
+                    <button className="settings-nav-btn" id="theme-btn">
+                        theme
+                    </button>
+                </div>
                 <div id="settings-content">
-                    <p>settings</p>
+                    
                 </div>
             </div>
         </>
