4726b4bed169c0eabfd780662a7f6bf3b76403cf
kstewar1 <77819538+kstewar1@users.noreply.github.com>
2022-11-25 08:56:43 -0500

    styling tweaks

--- src/modules/settingsPage/themesList.js
+++ src/modules/settingsPage/themesList.js
@@ -2,17 +2,19 @@
 [backgroundColor, boardColor, squareColor, squareTextColor, buttonColor,
     buttonTextColor, selectedColor, initialSquareColor]
 ["red", "blue", "green", "purple", "orange", "yellow", "pink", "black"]
+
+    - initial square text dark, regular square text light
 */
 
 export const themeVals = [
-    ["brunswick green", ["#28524a", "#d4bae2", "#519085", "#d385fc", "#3d7b6f",
-        "#90c6cb", "#a84f4882", "#2f3437"]],
-    ["antique brass", ["#ce826f", "#66674e", "#a17878", "#5f5f53", "#d26e59",
-        "#5a3d3d", "#825289b3", "#e9ebe0"]],
+    ["brunswick green", ["#28524a", "#edd2fb", "#519085", "#ffffff", "#3d7b6f",
+        "#90c6cb", "#f8a2ff87", "#2f3437"]],
+    ["antique brass", ["#ce826f", "#66674e", "#966d5d", "#395948", "#d26e59",
+        "#5a3d3d", "#528964b3", "#bebfb7"]],
     ["comet", ["#636583", "#a374e2", "#5e5990", "#d6d6d6", "#5e5990",
         "#e8e8e8", "#fe879c80", "#353535"]],
     ["gulf stream", ["#78b2ae", "#5967c3", "#54928e", "#cc9600", "#b0b9ac",
         "#4d4d4d", "#ffee846e", "#e3e3e3"]],
     ["harvest gold", ["#d79233", "#3a5c69", "#aa7122", "#92bef4", "#dda455", 
-        "#b45e2d", "#a2b5cd4f", "#2b2b2b"]]
+        "#b45e2d", "#ad9b28d6", "#2b2b2b"]]
 ]
