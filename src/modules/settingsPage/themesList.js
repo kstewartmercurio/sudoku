cf88f787fa5d362d6dd189f38a646eb68676f767
kstewar1 <77819538+kstewar1@users.noreply.github.com>
2022-11-24 21:45:02 -0500

    styling adjustments

--- src/modules/settingsPage/themesList.js
+++ src/modules/settingsPage/themesList.js
@@ -1,12 +1,7 @@
 /*
 [backgroundColor, boardColor, squareColor, squareTextColor, buttonColor,
     buttonTextColor, selectedColor, initialSquareColor]
-
-things to keep in mind:
-    - if top button bar text is dark then initial square text should be light,
-      and vice versa
 */
-
 
 export const themeVals = [
     ["manatee", ["#9c9c9e", "#5d5e6f", "#4c4b58", "#cbad7f", "#848484", 
@@ -15,6 +10,6 @@ export const themeVals = [
         "#5a3d3d", "#825289cc", "#e9ebe0"]],
     ["comet", ["#636583", "#a374e2", "#5e5990", "#d6d6d6", "#5e5990",
         "#e8e8e8", "#fe879cbf", "#353535"]],
-    ["gulf stream", ["#78b2ae", "#5967c3", "#54928e", "#cc9600", "#b3bcae",
+    ["gulf stream", ["#78b2ae", "#5967c3", "#54928e", "#cc9600", "#b0b9ac",
         "#4d4d4d", "pink", "#e3e3e3"]]
 ]
