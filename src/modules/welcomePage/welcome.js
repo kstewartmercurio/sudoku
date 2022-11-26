203d708b55e3578024849b3dfa576a2da58ab646
kstewar1 <77819538+kstewar1@users.noreply.github.com>
2022-11-26 13:00:17 -0500

    added more content to welcome page

--- src/modules/welcomePage/welcome.js
+++ src/modules/welcomePage/welcome.js
@@ -13,13 +13,32 @@ export function Welcome() {
                     </p>
                 </div>
 
-                <p id="p1">welcome to my sudoku app! i've made this web app for
-                    you to play sudoku. the board above allows you to play
-                    puzzles at whatever difficulty you'd like and there's extra
-                    functionality for making notes, getting hints, timing
-                    yourself, solving the puzzle, generating new puzzles, and
-                    clearing the board.
-                </p>
+                <div id="welcome-content">
+                    <p className="subheader">project overview</p>
+                    <p id="p1">
+                        welcome to my sudoku app! i've made this web app for
+                        you to play sudoku. the board above allows you to play
+                        puzzles at whatever difficulty you'd like and there's extra
+                        functionality for making notes, getting hints, timing
+                        yourself, solving the puzzle, generating new puzzles, and
+                        clearing the board.
+                    </p>
+                    <p className="subheader">tools used</p>
+                    <p id="p2">
+                        i used HTML, CSS, and JavaScript for the frontend
+                        design, npm for package management, Bootstrap for 
+                        button icons, and Git for version control.
+                    </p>
+                    <div id="icon-bar">
+                        this is where icons will go
+                    </div>
+                    <p id="p3">
+                        additionally, i used Coolors to generate color palletes
+                        for themes, uglkjgj's sudoku board api for puzzle
+                        generation, and monkeytype's website for design
+                        inspiration.
+                    </p>
+                </div> 
             </div>
         </>
     )
