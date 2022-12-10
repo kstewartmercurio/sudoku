506b342fc53ef9903cd7161685eb550ad604e2cd
kstewar1 <77819538+kstewar1@users.noreply.github.com>
2022-12-09 21:05:46 -0500

    fixed and back to generating 9x9 puzzles, still need to updated solver/puzzle.js to work with 6x6 puzzles

--- src/solver/nine.js
+++ src/solver/nine.js
@@ -22,7 +22,8 @@ export class Nine {
     checkForInvalidContents() {
         let contentsDict = {}
         for (let i = 0; i < this.contents.length; i++) {
-            if ((this.contents[i].getVal() in contentsDict) === true) {
+            if (((this.contents[i].getVal() in contentsDict) === true) &&
+                (this.contents[i].getVal() !== null)) {
                 return true;
             } else {
                 contentsDict[this.contents[i].getVal()] = true;
