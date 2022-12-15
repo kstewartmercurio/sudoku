81e93f28c33a9ef5a353648fab24b8dac79cfd2a
kstewar1 <77819538+kstewar1@users.noreply.github.com>
2022-12-14 20:30:17 -0500

    adding credit

--- src/components/boardPage/navbar.js
+++ src/components/boardPage/navbar.js
@@ -5,6 +5,11 @@ export function Navbar(props) {
         <div id="board-page-navbar">
             <p id="logo">You<span id="logo-accent">Do</span>Sudoku</p>
             <div id="board-page-nav-btn-bar">
+                <a className="board-page-nav-btn" 
+                    href="https://github.com/kstewartmercurio/sudoku" 
+                    rel="noopener noreferrer" target="_blank">
+                    <i className="fa-solid fa-code"></i>
+                </a>
                 <button className="board-page-nav-btn" id="info-btn" 
                     onClick={(e) => {
                     props.shareNavbarClick("info");
